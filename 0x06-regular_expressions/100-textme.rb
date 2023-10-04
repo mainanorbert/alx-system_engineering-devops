4ffb94acb9ab0578537c7e0580689a823ebaf2a2
mainanorbert <mainanorbert@gmail.com>
2023-10-04 08:23:48 +0000

    textme text extraction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# [ used to match opening bracket and ] to match closing bracket
+# \ is used to escape [ as \[ and ] as \]
 puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
