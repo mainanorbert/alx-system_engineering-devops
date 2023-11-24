d604d85cb5e9bd468521a62781a079a42e5a46ba
mainanorbert <mainanorbert@gmail.com>
2023-11-24 19:16:57 +0000

    installing flask from pip 3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Installing Flask version 2.1.0 using pip3
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    path    => ['/usr/bin'],
-    unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
-    require => Package['python3-pip'],
-}
-  package { 'python3-pip':
-    ensure => installed,
-}
+# installing flask from pip3 with puppet
 
+exec { 'install_flask':
+  command   => '/usr/bin/pip3 install Flask==2.1.0',
+}
