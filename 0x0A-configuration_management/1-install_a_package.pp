6f5906b1a8814f654dc8cbc08aab57b2cf4176d9
Norberts <mainanorbert@gmail.comm>
2023-11-29 22:19:27 +0300

    installing package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # installing flask with puppet
-   package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
 
-  package { 'werkzeug':
-    ensure   => '2.1.1',
-    provider => 'pip3'
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
