69c608f5002f568c6c95e3d3e709f2011645789d
Norberts <mainanorbert@gmail.comm>
2023-12-04 23:01:28 +0300

    configure with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,18 @@
-# 2-puppet_custom_http_response_header.pp
+# Installing Nginx and configuration
+# The name of the custom HTTP header must be X-Served-By
+# The value of the custom HTTP header must be the hostname of the server Nginx is running on
+  package { 'nginx':
+    ensure => installed,
+  }
 
-package { 'nginx':
-  ensure => installed,
-}
+  file { '/etc/nginx/http_header.conf':
+    ensure  => present,
+    content => "add_header X-Served-By ${hostname};",
+    notify  => Service['nginx'],
+  }
 
-file { '/etc/nginx/nginx.conf.backup':
-  ensure  => present,
-  source  => '/etc/nginx/nginx.conf',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/conf.d/custom_http_response_header.conf':
-  ensure  => present,
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        add_header X-Served-By $hostname;
-    }
-  }",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => File['/etc/nginx/conf.d/custom_http_response_header.conf'],
-}
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+  }
