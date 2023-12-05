f769809ebee423a191654282a26e1d83913f6bd5
Norberts <mainanorbert@gmail.comm>
2023-12-05 23:21:16 +0300

    using puppet to update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,58 @@
-# Installing Nginx and configuration
-# The name of the custom HTTP header must be X-Served-By
-# The value of the custom HTTP header must be the hostname of the server Nginx is running on
-  package { 'nginx':
-    ensure => installed,
-  }
+# configuring nginx
 
-  file { '/etc/nginx/http_header.conf':
-    ensure  => present,
-    content => "add_header X-Served-By ${hostname};",
-    notify  => Service['nginx'],
-  }
+# Update package lists
+exec { 'apt-get-update':
+  command     => 'apt-get update',
+  path        => '/usr/bin',
+  refreshonly => true,
+}
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
+# Install Nginx
+package { 'nginx':
+  ensure => 'installed',
+  require => Exec['apt-get-update'],
+}
+
+# Create directory and files
+file { '/var/www/html':
+  ensure => 'directory',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page",
+}
+
+# Configure Nginx
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => 'link',
+  target => '/etc/nginx/sites-available/default',
+  notify => Service['nginx'],
+}
+
+# Nginx service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => File['/etc/nginx/sites-enabled/default'],
+}
+
+# Notify a script to restart Nginx after configuration changes
+exec { 'nginx-restart':
+  command     => 'service nginx restart',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
+}
