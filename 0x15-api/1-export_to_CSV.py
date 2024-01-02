687635447e1d4f36160aba1d6bf446373447cd1d
Norberts <mainanorbert@gmail.comm>
2024-01-02 17:33:44 +0300

    exporting data in csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""using RESR API to return TO DO LIST for given employee"""
+"""using REST API to return csv file for given employee details"""
 import requests
 import sys
 import csv
@@ -10,17 +10,15 @@ def employee_todo_progress(employee_id):
     todo_url = f"{base_url}/{employee_id}/todos"
     user_url = f"{base_url}/{employee_id}"
     user_data = requests.get(user_url).json()
-    employee_name = user_data.get("name")
-    user_id = user_data.get("id")
+    # user_id = user_data.get("id")
     username = user_data.get('username')
     todo_data = requests.get(todo_url).json()
 
-    file_name = f"{user_id}.csv"
-    with open(file_name, mode='w', newline='') as f:
+    file_name = f"{employee_id}.csv"
+    with open(file_name, 'w', newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-        for task in todo_data:
-            writer.writerow([user_id, username, task.get('completed'),
-                            task.get('title')])
+        [writer.writerow([employee_id, username, task.get('completed'),
+                          task.get('title')]) for task in todo_data]
 
 
 if __name__ == "__main__":
