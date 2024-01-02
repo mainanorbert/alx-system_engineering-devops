3aded5580c27194bb48b82583244eeb389b95fd5
Norberts <mainanorbert@gmail.comm>
2024-01-02 23:18:13 +0300

    api json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,6 +5,35 @@ import requests
 import sys
 
 
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    user_id = sys.argv[1]
+    user = '{}users/{}'.format(base_url, user_id)
+    user_res = requests.get(user)
+    user_obj = user_res.json()
+    username = user_obj.get('username')
+
+    todos = '{}todos?userId={}'.format(base_url, user_id)
+    todo_res = requests.get(todos)
+    todo_obj = todo_res.json()
+
+    tasks = []
+    for task in todo_obj:
+        task_json = {"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": username}
+        tasks.append(task_json)
+
+    task = {str(user_id): tasks}
+    filename = '{}.json'.format(user_id)
+    with open(filename, 'w') as f:
+        json.dump(task, f)
+"""import json
+import requests
+import sys
+
+
 if __name__ == "__main__":
     employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com/users"
@@ -20,4 +49,4 @@ if __name__ == "__main__":
                               "completed": task["completed"],
                               "username": username} for task in todo_data]}
     with open(file_name, "w") as f:
-        json.dump(my_data, f)
+        json.dump(my_data, f)"""
