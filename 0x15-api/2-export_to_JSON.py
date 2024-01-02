01404215a63b6e440dc648f2da8cc3f608417848
Norberts <mainanorbert@gmail.comm>
2024-01-02 19:58:58 +0300

    json file

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -9,16 +9,16 @@ def employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com/users"
     todo_url = f"{base_url}/{employee_id}/todos"
     user_url = f"{base_url}/{employee_id}"
-    
-    # fetching user data
+
     user_data = requests.get(user_url).json()
-    
-    # fetching user todo work
     todo_data = requests.get(todo_url).json()
-    
+    username = user_data.get('username')
+
     file_name = f"{employee_id}.json"
-    my_data = {employee_id: [{"task": task["title"], "completed": task["completed"], "username": task["username"]} for task in todo_data]}
-    with open(file_name, m="w") as f:
+    my_data = {employee_id: [{"task": task["title"],
+                              "completed": task["completed"],
+                              "username": username} for task in todo_data]}
+    with open(file_name, "w") as f:
         json.dump(my_data, f)
 
 
