ed21ad83a9f3fd1fc8fbe3d46804b390bd4211cb
Norberts <mainanorbert@gmail.comm>
2024-01-03 05:41:04 +0300

    exporting api data to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,31 +20,3 @@ if __name__ == "__main__":
         [writer.writerow(
             [user_id, username, task.get("completed"), task.get("title")]
          ) for task in todos]
-"""if __name__ == '__main__':
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
-    user_id = sys.argv[1]
-    user = '{}users/{}'.format(base_url, user_id)
-    user_res = requests.get(user)
-    user_obj = user_res.json()
-    username = user_obj.get('username')
-
-    todos = '{}todos?userId={}'.format(base_url, user_id)
-    todo_res = requests.get(todos)
-    todo_obj = todo_res.json()
-
-    tasks = []
-    for task in todo_obj:
-        tasks.append([user_id,
-                      username,
-                      task.get('completed'),
-                      task.get('title')])
-
-    filename = '{}.csv'.format(user_id)
-    with open(filename, 'w') as f:
-        data = csv.writer(f, delimiter=',',
-                          quotechar='"',
-                          quoting=csv.QUOTE_ALL)
-
-        for task in tasks:
-            data.writerow(task)"""
