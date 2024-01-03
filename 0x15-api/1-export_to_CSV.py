aaa2d13b97b4fd44410981bd777e63365ddadb0b
Norberts <mainanorbert@gmail.comm>
2024-01-03 05:33:35 +0300

    using api and storing in csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,16 +10,16 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(user_id)).json()
     username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    with open("{}.csv".format(user_id), "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+            [user_id, username, task.get("completed"), task.get("title")]
+         ) for task in todos]
 """if __name__ == '__main__':
     base_url = 'https://jsonplaceholder.typicode.com/'
 
