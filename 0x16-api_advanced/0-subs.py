021f8b956a94a0b98a96b3f6bb334a42915b0fba
Norberts <mainanorbert@gmail.comm>
2024-01-16 15:55:33 +0300

    return no. of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """querying reddit api"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'CustomUserAgent'}
+    headers = {'User-Agent': ' Ubuntu 22.04.2 LTS'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         response = response.json()
