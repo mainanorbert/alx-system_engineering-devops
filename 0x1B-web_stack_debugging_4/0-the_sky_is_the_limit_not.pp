537e829c15a6a7a66f089280e092d36d7123b8fc
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:42:15 +0300

    nginx config

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # increasing traffic in nginx
 exec { 'fixing-nginx':
-  command => 'sudo sed -i \'s/15/3096/\' /etc/default/nginx',
+  command => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx',
   path    => '/usr/bin:/bin/',
 } ->
 
