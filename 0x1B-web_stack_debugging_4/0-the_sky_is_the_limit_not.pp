6102302b40a3b7a956c12a87d094b3ca08623b4c
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:49:33 +0300

    config apache

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixing error request in apache
 
 exec { 'error request fix':
-  command  => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx && sudo service nginx restart',
+  command  => 'sudo sed -i \'s/15/40000/\' /etc/default/nginx && sudo service nginx restart',
   provider => shell,
 }
