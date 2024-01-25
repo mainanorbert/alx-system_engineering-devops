55e0e522ee7996764db3b854bfd2f27d2adb2bee
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:57:10 +0300

    increase file limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,12 @@
 # and open files without error
 
 # increase hard file limit
-exec { 'increase file limit':
+exec { 'increase-file-hard-limit':
   command => 'sed -i \'/holberton hard/s/5/50000/\' /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/bin:/bin/',
 }
 
-# increase soft limit
+# increase-file-soft-limit
 exec { 'increase file limit':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/',
