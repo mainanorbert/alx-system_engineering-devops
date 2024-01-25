5ab054837ee27aa7fd62018e2a544725a5044610
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:13:05 +0300

    configure apacge

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # increasing traffic in nginx
 exec { 'fixing-nginx':
-  command => 'sudo sed -i \`s/15/3096/\` /etc/defualt/nginx',
+  command => 'sudo sed -i \'s/15/3096/\' /etc/defualt/nginx',
   path    => '/usr/bin/bin/:/bin/'
 } ->
 
 # restarting nginx
-exec { 'restarting-nginx'
+exec { 'restarting-nginx':
   command => 'sudo service nginx restart',
   path    => '/etc/init.d'
 }
