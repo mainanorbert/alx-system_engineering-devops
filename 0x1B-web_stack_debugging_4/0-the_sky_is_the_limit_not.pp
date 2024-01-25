7dc46626fd189908ea6dfb1ab814e957357af04d
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:48:21 +0300

    nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
-# increasing traffic in nginx
-exec { 'fixing-nginx':
-  command => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx',
-  path    => '/usr/bin:/bin/',
-}
+# fixing error request in apache
 
-# restarting nginx
-exec { 'restarting-nginx':
-  command => 'sudo service nginx restart',
-  path    => '/etc/init.d',
+exec { 'error request fix':
+  command  => 'sudo sed -i \'s/15/30000/\' /etc/default/nginx && sudo service nginx restart',
+  provider => shell,
 }
