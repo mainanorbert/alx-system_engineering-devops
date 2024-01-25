4cfcf84ac3212ce7f869419a74dee2340ead7864
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:19:25 +0300

    adding a path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # increasing traffic in nginx
 exec { 'fixing-nginx':
-  command => 'sudo sed -i \'s/15/3096/\' /etc/defualt/nginx',
-  path    => '/usr/bin/:/bin/'
+  command => 'sudo sed -i \'s/15/3096/\' /etc/default/nginx',
+  path    => '/usr/bin:/bin/',
 } ->
 
 # restarting nginx
 exec { 'restarting-nginx':
   command => 'sudo service nginx restart',
-  path    => '/etc/init.d'
+  path    => '/etc/init.d',
 }
