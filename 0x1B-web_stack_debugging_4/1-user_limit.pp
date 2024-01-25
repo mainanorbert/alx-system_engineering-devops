d452a94101d180bb1ec93fd7291c3940d24abc28
Norberts <mainanorbert@gmail.comm>
2024-01-25 16:53:10 +0300

    OS configuration so that it is possible to login

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,12 +3,12 @@
 
 # increase hard file limit
 exec { 'increase file limit':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i \'/holberton hard/s/5/50000/\' /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # increase soft limit
 exec { 'increase file limit':
   command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
 }
